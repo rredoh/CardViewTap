b5d3a7a52958487822c5d6f66df5850c793e96b9
Reinardus Redo Haryono <rredoh.rrh@gmail.com>
2018-11-05 16:57:21 +0700

    Init

---
+++ CardViewTap/CardViewTap.Android/obj/Debug/MonoAndroid90/android/src/md5fd370a847382c1e56c4360c1497a638c/MainActivity.java
@@ -0,0 +1,47 @@
+package md5fd370a847382c1e56c4360c1497a638c;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("CardViewTap.Droid.MainActivity, CardViewTap.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("CardViewTap.Droid.MainActivity, CardViewTap.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
